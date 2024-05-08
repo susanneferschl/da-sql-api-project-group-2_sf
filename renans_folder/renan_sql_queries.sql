6f3297474a9f5546d0afbd6364b739ba94f64b6f
RSR <rnansoares@gmail.com>
2024-05-08 17:59:00 +0200

    Renan Wed EOB

---
+++ renans_folder/renan_sql_queries.sql
@@ -0,0 +1,45 @@
+/*Merging 2010 flights with airports*/
+SELECT COUNT(*)
+FROM flights_api_rr
+WHERE origin IN ('DCA', 'IAD', 'BWI', 'PHL', 'EWR', 'JFK', 'LGA')
+   OR dest IN ('DCA', 'IAD', 'BWI', 'PHL','EWR', 'JFK', 'LGA');
+
+SELECT COUNT(*)
+FROM flights_api_rr;
+
+CREATE TABLE flights_group_x_rr AS
+(SELECT fa.*, 
+	   origin.lat, 
+	   origin.lon, 
+	   origin.city,
+	   origin.tz AS tz_origin,
+	   dest.tz AS tz_dest
+FROM flights_api_rr AS fa
+LEFT JOIN airports AS origin
+ON fa.origin = origin.faa
+LEFT JOIN airports AS dest
+ON fa.dest = dest.faa);
+
+/*End of Merging*/
+
+SELECT *
+FROM flights_2010_to_2017 ft;
+
+SELECT DATE_PART('year', flight_date) AS year,
+       AVG(cancelled::int) AS cancellation_rate
+FROM flights_2010_to_2017
+GROUP BY DATE_PART('year', flight_date)
+ORDER BY year;
+
+/*Get the cancellation rate as % for each year.
+ * Cancellation rate = count of flights cancelled / count of flights */
+
+SELECT DATE_PART('year', flight_date) AS year,
+       COUNT(*) AS flight_count,
+       SUM(cancelled::int) AS cancelled_flights,
+       ROUND(AVG(cancelled::int) * 100, 2) || '%' AS cancellation_rate
+FROM flights_2010_to_2017
+GROUP BY DATE_PART('year', flight_date)
+ORDER BY year;
+
+
