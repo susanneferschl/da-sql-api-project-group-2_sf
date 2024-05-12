9c8befe3e82600e5843defc5f98f59c5d8360863
Trina Kohestani <trinakohestani@Trinas-Air.box>
2024-05-12 18:39:53 +0200

    Clean version

--- project-work-clean/flights_airports.sql
+++ project-work-clean/flights_airports.sql
@@ -1,3 +1,25 @@
+
+SELECT COUNT(*)
+FROM flights_api
+WHERE origin IN ('DCA', 'IAD', 'BWI', 'PHL', 'EWR', 'JFK', 'LGA')
+   OR dest IN ('DCA', 'IAD', 'BWI', 'PHL','EWR', 'JFK', 'LGA');
+
+SELECT COUNT(*)
+FROM flights_api;
+
+CREATE TABLE flights_group_x AS
+(SELECT fa.*, 
+	   origin.lat, 
+	   origin.lon, 
+	   origin.city,
+	   origin.tz AS tz_origin,
+	   dest.tz AS tz_dest
+FROM flights_api AS fa
+LEFT JOIN airports AS origin
+ON fa.origin = origin.faa
+LEFT JOIN airports AS dest
+ON fa.dest = dest.faa);
+
 /*Merging 2010 flights with airports*/
 SELECT COUNT(*)
 FROM flights_api_rr
